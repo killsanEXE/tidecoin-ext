84bec70855c17f44becda322ec7b3707e1c411d7
killsanEXE <killsan.exe@gmail.com>
2023-09-25 13:05:55 +0300

    added something

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -8,9 +8,34 @@ import { setupWalletProxy } from '@/ui/utils/setup';
 import { useAppState } from './states/appState';
 import { useWalletState } from './states/walletState';
 import { guestRouter, authenticatedRouter } from "@/ui/pages/router";
+import { IWallet, IWalletController } from '@/shared/interfaces';
 
 export default function App() {
-  const [ router, setRouter ] = useState<Router>(guestRouter);
+
+  const login = async (walletController: IWalletController) => {
+    const exportedWallets = await walletController.importWallets("1");
+    exportedWallets[0].accounts[0].address =
+      await walletController.loadAccountPublicAddress(
+        exportedWallets[0],
+        exportedWallets[0].accounts[0]
+      );
+    const map = new Map<number, IWallet>();
+    exportedWallets.forEach((f) => map.set(f.id, f))
+    updateWalletState({
+      wallets: map,
+      currentWallet: {
+        ...exportedWallets[0],
+        currentAccount: exportedWallets[0].accounts[0],
+      },
+    });
+    updateAppState({
+      isUnlocked: true,
+      password: "1",
+    });
+  };
+
+
+  const [router, setRouter] = useState<Router>(guestRouter);
 
   const { isReady, isUnlocked, updateAppState } = useAppState((v) => ({
     isReady: v.isReady,
@@ -28,18 +53,20 @@ export default function App() {
       const walletController = setupWalletProxy();
       updateWalletState({ controller: walletController, vaultIsEmpty: await walletController.isVaultEmpty() });
       updateAppState({ isReady: true });
+
+      login(walletController)
     }
 
     if (!isReady) setupApp();
     else if (isReady && isUnlocked) setRouter(authenticatedRouter);
-  }, [ isReady, isUnlocked, setupWalletProxy, updateWalletState, updateAppState, router, setRouter ]);
+  }, [isReady, isUnlocked, setupWalletProxy, updateWalletState, updateAppState, router, setRouter]);
 
   return (
     <div className='app'>
-      {isReady ? <RouterProvider router={router}/> : <ReactLoading type="spin" color="#fff"/>}
+      {isReady ? <RouterProvider router={router} /> : <ReactLoading type="spin" color="#fff" />}
       <Toaster position="bottom-center" toastOptions={{
         className: "toast"
-      }}/>
+      }} />
     </div>
   );
 }
