a107eb49f1cace983819ef89fb6a66c832c9f66a
killsanEXE <killsan.exe@gmail.com>
2023-09-28 15:05:01 +0300

    something

--- src/ui/components/pages/component.tsx
+++ src/ui/components/pages/component.tsx
@@ -1,11 +1,33 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import s from "./styles.module.scss";
 import cn from "classnames";
 import ArrowLeft from "@/ui/components/icons/ArrowLeft";
 import PlusInCircleIcon from "@/ui/components/icons/PlusInCirlceIcon";
+import { useEffect } from "react";
 
 export default function PagesLayout() {
 
+    const routeTitles = {
+        "/pages/switch-account": {
+            "title": "Switch Account",
+            "action": () => { navigate("/pages/create-new-account ") },
+        },
+        "/pages/create-new-account": {
+            "title": "Create New Account",
+        },
+        "/pages/change-password": {
+            "title": "Change Password",
+        },
+        "/pages/receive": {
+            "title": "Receive TDC",
+        },
+        "/pages/switch-wallet": {
+            "title": "Switch Wallet",
+            "action": () => { console.log("CREATE NEW WALLET ") },
+        }
+    }
+
+    const currentRoute = useLocation();
     const navigate = useNavigate();
 
     return (
@@ -19,15 +41,16 @@ export default function PagesLayout() {
                 >
                     <ArrowLeft /> Back
                 </p>
-                <p className={s.controlElem}>Switch account</p>
-                <p
-                    className={cn(s.controlElem, s.addNew)}
-                    onClick={() => {
-                        navigate("/pages/create-new-account");
-                    }}
-                >
-                    <PlusInCircleIcon />
-                </p>
+                <p className={s.controlElem}>{routeTitles[currentRoute.pathname]["title"]}</p>
+                {
+                    routeTitles[currentRoute.pathname]["action"] === undefined ? <p></p> :
+                        <p
+                            className={cn(s.controlElem, s.addNew)}
+                            onClick={routeTitles[currentRoute.pathname]["action"]}
+                        >
+                            <PlusInCircleIcon />
+                        </p>
+                }
             </div>
             <div className={s.contentDiv}>
                 <Outlet />
