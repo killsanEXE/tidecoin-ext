d8a97cacf626f9de2c7aa8d2740084b4d7341bb7
Kiel Ed <kieledssh@gmail.com>
2023-09-29 13:12:44 +0300

    added crypting

--- src/background/services/storage.ts
+++ src/background/services/storage.ts
@@ -2,12 +2,7 @@ import {
   browserStorageLocalGet,
   browserStorageLocalSet,
 } from "@/shared/utils/browser";
-import {
-  bytesToHex as toHex,
-  hexToBytes as fromHex,
-} from "@noble/hashes/utils";
-import { pbkdf2Async } from "@noble/hashes/pbkdf2";
-import { sha256 } from "@noble/hashes/sha256";
+import * as encryptorUtils from "@metamask/browser-passworder";
 import { IWallet } from "@/shared/interfaces";
 
 class StorageService {
@@ -23,90 +18,35 @@ class StorageService {
         })),
       };
     });
-    const { encryptedData, iv, salt } = await this.encryptData(
+    const encrypted = await this.encryptData(
       password,
       JSON.stringify(walletsToSave)
     );
 
-    await browserStorageLocalSet({ vault: encryptedData, iv, salt });
+    await browserStorageLocalSet(encrypted);
   }
 
   async getLocalValues() {
     const data = (await browserStorageLocalGet(undefined)) as any;
-    return data as { iv: string; vault: string; salt: string };
+    return data as string | undefined;
   }
 
   async importWallets(password: string): Promise<IWallet[]> {
-    const { salt, iv, vault } = await this.getLocalValues();
-    const decrypted = await this.decryptData(password, vault, salt, iv);
-    const wallets: IWallet[] = JSON.parse(decrypted);
-    return wallets.map((i) => ({ ...i, id: i.id }));
-  }
-
-  private async importKey(
-    password: string,
-    encrypt: boolean,
-    salt: Uint8Array
-  ) {
-    const keyHash = await pbkdf2Async(
-      sha256,
-      new TextEncoder().encode(password),
-      salt,
-      {
-        c: 50000,
-        dkLen: 32,
-      }
-    );
-    return await crypto.subtle.importKey(
-      "raw",
-      keyHash,
-      { name: "AES-GCM" },
-      false,
-      [encrypt ? "encrypt" : "decrypt"]
-    );
+    const encrypted = await this.getLocalValues();
+    if (!encrypted) return [];
+    const decrypted: IWallet[] = (await this.decryptData(
+      password,
+      encrypted
+    )) as any;
+    return decrypted.map((i, index) => ({ ...i, id: index }));
   }
 
   private async encryptData(password: string, data: string) {
-    const salt = crypto.getRandomValues(new Uint8Array(16));
-    const iv = crypto.getRandomValues(new Uint8Array(12));
-
-    const dataBuffer = new TextEncoder().encode(data);
-
-    const encryptedData = await crypto.subtle.encrypt(
-      {
-        name: "AES-GCM",
-        iv,
-      },
-      await this.importKey(password, true, salt),
-      dataBuffer
-    );
-
-    return {
-      encryptedData: toHex(new Uint8Array(encryptedData)),
-      salt: toHex(new Uint8Array(salt)),
-      iv: toHex(new Uint8Array(iv)),
-    };
+    return await encryptorUtils.encrypt(password, data);
   }
 
-  private async decryptData(
-    password: string,
-    encryptedData: string,
-    salt: string,
-    iv: string
-  ) {
-    const encryptedDataBuffer = fromHex(encryptedData);
-
-    const decryptedData = await crypto.subtle.decrypt(
-      {
-        name: "AES-GCM",
-        iv: fromHex(iv),
-      },
-      await this.importKey(password, false, fromHex(salt)),
-      encryptedDataBuffer
-    );
-
-    const decoder = new TextDecoder();
-    return decoder.decode(decryptedData);
+  private async decryptData(password: string, encryptedData: string) {
+    return await encryptorUtils.decrypt(password, encryptedData);
   }
 }
 
