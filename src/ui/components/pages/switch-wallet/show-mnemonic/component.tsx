47e7685455577acb7379e9ccd6c14d2e11dac52d
killsanEXE <killsan.exe@gmail.com>
2023-10-04 13:44:57 +0300

    show mnemonic styles

--- src/ui/components/pages/switch-wallet/show-mnemonic/component.tsx
+++ src/ui/components/pages/switch-wallet/show-mnemonic/component.tsx
@@ -3,6 +3,8 @@ import { useWalletState } from "@/ui/states/walletState";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import s from "./styles.module.scss";
+import CopyIcon from "@/ui/components/icons/CopyIcon";
+import { copyToClipboard } from "@/ui/utils";
 
 const ShowMnemonic = () => {
     const [unlocked, setUnlocked] = useState(false);
@@ -10,9 +12,18 @@ const ShowMnemonic = () => {
     const { wallets } = useWalletState((v) => ({ wallets: v.wallets }))
 
     return (
-        <div className={s.ShowMnemonic}>
+        <div className={s.showMnemonic}>
             {unlocked ?
-                <div>{wallets.get(Number(walletId))?.phrase}</div>
+                <div className={s.phraseDiv}>
+                    <div className={s.phraseWrapper}>
+                        {wallets.get(Number(walletId))?.phrase.split(" ").map((word, index) =>
+                            <div key={index} className={s.word}>{index + 1}. <p className={s.wordWord}>{word}</p></div>
+                        )}
+                    </div>
+                    <div className={s.copyDiv} onClick={() => {
+                        copyToClipboard(wallets.get(Number(walletId))?.phrase);
+                    }}><CopyIcon /> Copy</div>
+                </div>
                 : <CheckPassword handler={() => { setUnlocked(true) }} />}
         </div>
     );
