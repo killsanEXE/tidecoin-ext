0feb8d990dc3ba84e2f69e46a531b6beee870dde
killsanEXE <killsan.exe@gmail.com>
2023-10-06 13:39:09 +0300

    bad styles

--- src/ui/components/pages/new-wallet/restore-mnemonic/component.tsx
+++ src/ui/components/pages/new-wallet/restore-mnemonic/component.tsx
@@ -2,48 +2,14 @@ import s from "./styles.module.scss";
 import { useCreateNewWallet } from "@/ui/hooks/wallet";
 import { useControllersState } from "@/ui/states/controllerState";
 import { useWalletState } from "@/ui/states/walletState";
-import { useEffect, useState } from "react";
-import ReactLoading from "react-loading";
+import { useState } from "react";
 import cn from "classnames";
 import { useNavigate } from "react-router-dom";
-import CopyIcon from "@/ui/components/icons/CopyIcon";
-import { copyToClipboard } from "@/ui/utils";
 import toast from "react-hot-toast";
-
-// export interface IMnemonicInput {
-//   word1: string;
-//   word2: string;
-//   word3: string;
-//   word4: string;
-//   word5: string;
-//   word6: string;
-//   word7: string;
-//   word8: string;
-//   word9: string;
-//   word10: string;
-//   word11: string;
-//   word12: string;
-// }
 
 const RestoreMnemonic = () => {
 
-  // const mnemonicInput: IMnemonicInput = {
-  //   word1: '',
-  //   word2: '',
-  //   word3: '',
-  //   word4: '',
-  //   word5: '',
-  //   word6: '',
-  //   word7: '',
-  //   word8: '',
-  //   word9: '',
-  //   word10: '',
-  //   word11: '',
-  //   word12: ''
-  // };
-
   const [step, setStep] = useState(1);
-  const [savedPhrase, setSavedPhrase] = useState(false);
   const { updateWalletState } = useWalletState((v) => ({
     updateWalletState: v.updateWalletState,
   }));
@@ -51,6 +17,7 @@ const RestoreMnemonic = () => {
     walletController: v.walletController,
   }));
   const [mnemonicPhrase, setMnemonicPhrase] = useState<string[]>([
+    "",
     "",
     "",
     "",
@@ -82,13 +49,17 @@ const RestoreMnemonic = () => {
               <div className={s.phrase}>
                 {mnemonicPhrase.map((word, index) => (
                   <div key={index} className={s.word}>
-                    {index + 1}. <p className={s.wordWord}>
-                      <input type="text" value={mnemonicPhrase[index]} onChange={(e) => {
-                        const mnemPhrase = mnemonicPhrase;
-                        mnemPhrase[index] = e.target.value
-                        setMnemonicPhrase(mnemPhrase);
-                      }} />
-                    </p>
+                    {index + 1}.
+                    <input
+                      type="text"
+                      className="input"
+                      value={mnemonicPhrase[index]}
+                      onChange={(e) => {
+                        const updatedMnemonicPhrase = [...mnemonicPhrase];
+                        updatedMnemonicPhrase[index] = e.target.value;
+                        setMnemonicPhrase(updatedMnemonicPhrase);
+                      }}
+                    />
                   </div>
                 ))}
               </div>
@@ -97,7 +68,7 @@ const RestoreMnemonic = () => {
               <button
                 className={cn(s.continue, "btn", "primary")}
                 onClick={() => {
-                  if (!savedPhrase) toast("Save the phrase first");
+                  if (mnemonicPhrase.find(f => f.trim().length <= 0) !== undefined) toast("Please insert all the words");
                   else setStep(2);
                 }}
               >
@@ -111,10 +82,14 @@ const RestoreMnemonic = () => {
           <div className={s.continueWrapper}>
             <button
               onClick={async () => {
-                // await createNewWallet(mnemonicPhrase.join(" "));
+                try {
+                  await createNewWallet(mnemonicPhrase.join(" "));
+                } catch (e) {
+                  toast.error("Words you entered is invalid");
+                  setStep(1);
+                }
                 // updateWalletState({ vaultIsEmpty: false });
                 // navigate("/home/wallet");
-                console.log(mnemonicPhrase.join(" "))
               }}
               className={cn(s.continue, "btn", "primary")}
             >
