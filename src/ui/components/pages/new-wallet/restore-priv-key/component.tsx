3d9569652616127538dc21c507ded611b371bb06
killsanEXE <killsan.exe@gmail.com>
2023-10-09 15:46:00 +0300

    added recover from privKey page

--- src/ui/components/pages/new-wallet/restore-priv-key/component.tsx
+++ src/ui/components/pages/new-wallet/restore-priv-key/component.tsx
@@ -1,9 +1,28 @@
 // import s from "./styles.module.scss";
 
+import { useCreateNewWallet } from "@/ui/hooks/wallet";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const RestorePrivKey = () => {
+
+  const createNewWallet = useCreateNewWallet();
+  const navigate = useNavigate();
+
+  const [privKey, setPrivKey] = useState("");
+
+  const recoverWallet = async () => {
+    await createNewWallet(privKey, "simple");
+    navigate("/home/wallet")
+  }
 
   return (
     <form className="form" onSubmit={(e) => e.preventDefault()}>
+      <div className="form-field">
+        <span className="input-span">Enter your private key</span>
+        <input className="input" onChange={(e) => { setPrivKey(e.target.value) }} type="password" />
+      </div>
+      <button className="btn primary" onClick={recoverWallet}>Recover</button>
     </form>
   );
 };
