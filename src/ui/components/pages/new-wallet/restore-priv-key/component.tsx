72ec594d9018069178e3c53af91db60152c6528d
killsanEXE <killsan.exe@gmail.com>
2023-10-09 17:49:41 +0300

    recover from priv key fix

--- src/ui/components/pages/new-wallet/restore-priv-key/component.tsx
+++ src/ui/components/pages/new-wallet/restore-priv-key/component.tsx
@@ -1,6 +1,7 @@
 // import s from "./styles.module.scss";
 
 import { useCreateNewWallet } from "@/ui/hooks/wallet";
+import { useControllersState } from "@/ui/states/controllerState";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -8,12 +9,13 @@ const RestorePrivKey = () => {
 
   const createNewWallet = useCreateNewWallet();
   const navigate = useNavigate();
+  const { walletController } = useControllersState((v) => ({ walletController: v.walletController }))
 
   const [privKey, setPrivKey] = useState("");
 
   const recoverWallet = async () => {
-    const wallet = await createNewWallet(privKey, "simple");
-    console.log(wallet);
+    await createNewWallet(privKey, "simple");
+    await walletController.saveWallets();
     navigate("/home/wallet")
   }
 
