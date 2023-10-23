4700df021d248f1ff811bf2a0acea53f76b1e9ab
Kiel Ed <kieledssh@gmail.com>
2023-10-23 15:06:25 +0300

    refactoring layout and provider pages

--- src/ui/pages/provider/layout.tsx
+++ src/ui/pages/provider/layout.tsx
@@ -2,58 +2,15 @@ import { useControllersState } from "@/ui/states/controllerState";
 import s from "./styles.module.scss";
 import { FC, useEffect, useState } from "react";
 import ReactLoading from "react-loading";
-import { FingerPrintIcon, KeyIcon } from "@heroicons/react/24/solid";
-import { useLocation } from "react-router-dom";
-import { ApprovalData } from "@/shared/interfaces/notification";
-
-interface RouteOptions {
-  path: string;
-  documentTitle: string;
-  resolveBtnText?: string;
-  resolveBtnClassName: string;
-  content: React.ReactNode | ((...args: any[]) => React.ReactNode);
-  getArgs?: (approval: ApprovalData) => Promise<any[]>;
-}
-
-const routeProps: RouteOptions[] = [
-  {
-    path: "/provider/connect",
-    content: (
-      <>
-        <FingerPrintIcon className="w-40 h-40 text-green-400 bg-input-bg rounded-full p-4" />
-        <h3 className="text-xl font-medium">Access required</h3>
-      </>
-    ),
-    documentTitle: "Connect to tidecoin",
-    resolveBtnClassName: "bg-text text-bg hover:bg-green-500 hover:text-text",
-  },
-  {
-    path: "/provider/sign",
-    content: (message: string) => (
-      <>
-        <KeyIcon className="w-10 h-10 text-yellow-500" />
-        <h4 className="text-xl font-medium">Sign request</h4>
-        <div className="text-sm text-gray-400">You are signing</div>
-        <div className="p-2 bg-input-bg rounded-xl max-h-full">
-          <div className="break-all max-h-60 overflow-y-auto px-1">{message}</div>
-        </div>
-      </>
-    ),
-    documentTitle: "Sign transaction",
-    resolveBtnClassName: "bg-text text-bg hover:bg-yellow-500 hover:text-bg",
-    getArgs: async (approval) => {
-      return [approval.params.data.text];
-    },
-    resolveBtnText: "Sign",
-  },
-];
 
 interface Props {
   documentTitle: string;
-  content: React.ReactNode;
+  children: React.ReactNode;
+  resolveBtnText?: string;
+  resolveBtnClassName: string;
 }
 
-const Layout: FC<Props> = ({ content, documentTitle }) => {
+const Layout: FC<Props> = ({ children, documentTitle, resolveBtnClassName, resolveBtnText }) => {
   const [origin, setOrigin] = useState<string>();
 
   const { notificationController } = useControllersState((v) => ({
@@ -89,12 +46,10 @@ const Layout: FC<Props> = ({ content, documentTitle }) => {
       <div className="flex justify-center w-full mt-4">
         <div className="px-5 py-2 rounded-xl bg-input-bg text-gray-300">{origin}</div>
       </div>
-      <div className={s.content}>
-        {typeof currentRoute.content === "function" ? currentRoute.content(...args) : currentRoute.content}
-      </div>
+      <div className={s.content}>{children}</div>
       <div className={s.btnContainer}>
-        <button className={currentRoute.resolveBtnClassName} onClick={onResolve}>
-          {currentRoute.resolveBtnText ?? "Resolve"}
+        <button className={resolveBtnClassName} onClick={onResolve}>
+          {resolveBtnText ?? "Resolve"}
         </button>
         <button className="bg-bg border border-text hover:bg-red-500 hover:border-red-500" onClick={onReject}>
           Reject
--- src/ui/pages/provider/sign/component.tsx
+++ src/ui/pages/provider/sign/component.tsx
@@ -1,11 +1,10 @@
 import { useControllersState } from "@/ui/states/controllerState";
-import s from "./styles.module.scss";
 import { useEffect, useState } from "react";
 
 import { KeyIcon } from "@heroicons/react/24/solid";
+import Layout from "../layout";
 
 const Sign = () => {
-  const [origin, setOrigin] = useState<string>();
   const [message, setMessage] = useState<string>();
 
   const { notificationController } = useControllersState((v) => ({
@@ -13,46 +12,27 @@ const Sign = () => {
   }));
 
   useEffect(() => {
-    document.title = "Signing message";
     (async () => {
       const approval = await notificationController.getApproval();
       setMessage(approval.params.data.text);
-      setOrigin(approval.params.session.origin);
     })();
   }, []);
 
   return (
-    <div className={s.container}>
-      <div className="flex justify-center w-full mt-4">
-        <div className="px-5 py-2 rounded-xl bg-input-bg text-gray-300">{origin}</div>
-      </div>
-      <div className={s.content}>
+    <Layout
+      documentTitle="Sign message"
+      resolveBtnClassName="bg-text text-bg hover:bg-yellow-500 hover:text-bg"
+      resolveBtnText="Sign"
+    >
+      <>
         <KeyIcon className="w-10 h-10 text-yellow-500" />
         <h4 className="text-xl font-medium">Sign request</h4>
         <div className="text-sm text-gray-400">You are signing</div>
         <div className="p-2 bg-input-bg rounded-xl max-h-full">
           <div className="break-all max-h-60 overflow-y-auto px-1">{message}</div>
         </div>
-      </div>
-      <div className={s.btnContainer}>
-        <button
-          className="bg-text text-bg hover:bg-yellow-500 hover:text-bg"
-          onClick={() => {
-            notificationController.resolveApproval();
-          }}
-        >
-          Sign
-        </button>
-        <button
-          className="bg-bg border border-text hover:bg-red-500 hover:border-red-500"
-          onClick={() => {
-            notificationController.rejectApproval();
-          }}
-        >
-          Reject
-        </button>
-      </div>
-    </div>
+      </>
+    </Layout>
   );
 };
 
