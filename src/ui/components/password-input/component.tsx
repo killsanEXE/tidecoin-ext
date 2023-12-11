b24e558fe00eb6934bcc67c31c70f7ecce22d8b7
Kiel Ed <kieledssh@gmail.com>
2023-12-11 11:55:24 +0300

    added eye

--- src/ui/components/password-input/component.tsx
+++ src/ui/components/password-input/component.tsx
@@ -1,37 +1,53 @@
 import { t } from "i18next";
 import { FieldValues, Path, UseFormRegister } from "react-hook-form";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+import s from "./styles.module.scss";
 
 const PasswordInput = <T extends FieldValues>({
   label,
   name,
   register,
-  showSeparateLabel = true
+  showSeparateLabel = true,
 }: {
   register: UseFormRegister<T>;
   name: Path<T>;
   label: string;
-  showSeparateLabel?: boolean
+  showSeparateLabel?: boolean;
 }) => {
+  const [hidden, setHidden] = useState(true);
+
   return (
     <div className="form-field">
-      {showSeparateLabel &&
+      {showSeparateLabel && (
         <label className="input-span" htmlFor={name}>
           {label}
         </label>
-      }
-      <input
-        id={name}
-        {...register(name, {
-          minLength: {
-            value: 1,
-            message: t("components.password_input.should_be_more_than_1_symbol"),
-          },
-          required: true,
-        })}
-        type="password"
-        className="input"
-        placeholder={showSeparateLabel ? "" : label}
-      />
+      )}
+      <div className={s.inputWrapper}>
+        <input
+          id={name}
+          {...register(name, {
+            minLength: {
+              value: 1,
+              message: t("components.password_input.should_be_more_than_1_symbol"),
+            },
+            required: true,
+          })}
+          type={hidden ? "password" : "text"}
+          className="input w-full"
+          placeholder={showSeparateLabel ? "" : label}
+        />
+        <button
+          className={s.hideBtn}
+          onClick={(e) => {
+            e.preventDefault();
+            setHidden((p) => !p);
+          }}
+        >
+          {hidden ? <EyeIcon className={s.icon} /> : <EyeSlashIcon className={s.icon} />}
+        </button>
+      </div>
     </div>
   );
 };
